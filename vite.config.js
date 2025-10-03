b19fbf736bc6e16ef32a30e23af6e3e93b50d038
ghost-gilbert <khc07979@gmail.com>
2025-10-03 18:45:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/DU/",  // 👈 replace with your repo name
+  base: "./",  // 👈 replace with your repo name
 })
