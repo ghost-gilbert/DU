f72d16122054f2e77473488b7b5cb7af05fba88d
ghost-gilbert <khc07979@gmail.com>
2025-10-03 19:15:22 +0530

    hi

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "./",  // 👈 replace with your repo name
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  base: "./",
 })
